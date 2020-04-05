2a6cb63cb20e0bbf5691f9ac6659c73f9aa9a840
cugliubingyi <cugliubingyi@gmail.com>
2020-04-05 23:06:23 +0800

    add form validation

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const Input = props => {
+  let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+  
+  switch ( props.elementType ) {
+    case 'input':
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          value={props.value}
+          onChange={props.changed}
+          {...props.elementConfig}
+        />
+      );
+      break;
+    case 'textarea':
+      inputElement = (
+        <textarea
+          className={inputClasses.join(' ')}
+          value={props.value}
+          onChange={props.changed}
+          {...props.elementConfig}
+        />
+      );
+      break;
+    case 'select':
+      inputElement = (
+        <select
+          className={inputClasses.join(' ')}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          value={props.value}
+          onChange={props.changed}
+          {...props.elementConfig}
+        />
+      );
+  }
+
+  let validationError = null;
+  if (props.invalid && props.touched) {
+    validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>;
+  }
+
+  return (
+    <div className={inputClasses.join(' ')}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+      {validationError}
+    </div>
+  );  
+};
+
+export default Input;
