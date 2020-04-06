f1a182858d4426e88098ab17301ccc59ad5b1a27
cugliubingyi <cugliubingyi@gmail.com>
2020-04-06 17:18:04 +0800

    add overall validation to form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,9 +85,11 @@ class ContactData extends Component {
           ],
         },
         value: "",
-        valid: false,
+        validation: {},
+        valid: true
       },
     },
+    formIsValid: false,
     loading: false,
   };
 
@@ -128,8 +130,8 @@ class ContactData extends Component {
       isValid = value.length >= rules.minLength && isValid;
     }
 
-    if (rules.minLength) {
-      isValid = value.length <= rules.minLength && isValid;
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
     return isValid;
@@ -149,7 +151,12 @@ class ContactData extends Component {
     );
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    this.setState({ orderForm: updatedOrderForm });
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   };
 
   render() {
@@ -174,7 +181,7 @@ class ContactData extends Component {
             changed={(event) => this.handleInputChange(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
     if (this.state.loading) {
