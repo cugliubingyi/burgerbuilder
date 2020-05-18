7e087ac093ddc1fa7f65f215222e94f143f41fa6
bingyi liu <cugliubingyi@gmail.com>
2020-05-18 20:42:02 +0800

    add auth to project

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from "../../components/UI/Button/Button";
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.module.css';
 import * as actions from '../../store/actions';
 
@@ -38,6 +39,7 @@ class Auth extends Component {
         touched: false,
       },
     },
+    isSignup: true
   };
 
   checkValidity(value, rules) {
@@ -89,7 +91,13 @@ class Auth extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+  }
+
+  handleSwitchAuthMode = () => {
+    this.setState(prevState => {
+      return {isSignup: !prevState.isSignup};
+    });
   }
 
   render() {
@@ -101,7 +109,7 @@ class Auth extends Component {
       });
     }
 
-    const form = formElementsArray.map((formElement) => (
+    let form = formElementsArray.map((formElement) => (
       <Input
         key={formElement.id}
         elementType={formElement.config.elementType}
@@ -114,21 +122,47 @@ class Auth extends Component {
       />
     ));
 
+    if (this.props.loading) {
+      form = <Spinner />;
+    }
+
+    let errorMessage = null;
+
+    if (this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      );
+    }
+
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.handleSubmit}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
+        <Button
+          clicked={this.handleSwitchAuthMode}
+          btnType="Danger"
+        >
+          SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}
+        </Button>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error
+  }
+};
+
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
-  }
-}
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+  };
+};
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId
   };
 };
 
@@ -20,8 +23,41 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const logout = () => {
+  return {
+
+  } 
+};
+
+export const checkAuthTimeout = (expirationTime) => {
   return dispatch => {
-    dispatch(authStart());
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
   }
 }
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAjm4Aowl1qzBVW7X0Plj1IEEIVc9wkAPU';
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAjm4Aowl1qzBVW7X0Plj1IEEIVc9wkAPU';
+    }
+    axios.post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err.response.data.error));
+      })
+  };
+};
